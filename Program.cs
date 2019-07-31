e4334ebae7c8f2f3099e4e5c6bb9916c3f941395
罗君 <13760614423@163.com>
2019-07-31 17:59:40 +0800

    hello world

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.IO;
+using System.Timers;
+using System;
+
+namespace Hello
+{
+  class Program
+  {
+    static Timer tim = new Timer(1000 * 1);
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("开始");
+      tim.Elapsed += TimElapsed;
+      tim.Start();
+      Console.WriteLine("计数器开始");
+      var key = Console.ReadLine();
+      while (key != "exit")
+      {
+        key = Console.ReadLine();
+      }
+      Console.WriteLine("结束");
+    }
+
+    static void TimElapsed(object sender, ElapsedEventArgs e)
+    {
+      var fullPath = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName, "log");
+      if (!Directory.Exists(fullPath))
+      {
+        Directory.CreateDirectory(fullPath);
+      }
+      var fullName = Path.Combine(fullPath, string.Format("{0}.log", DateTime.Now.ToString("yyyyMMddHHmmssfff")));
+      using (StreamWriter sw = new StreamWriter(fullName, true, Encoding.UTF8))
+      {
+        var log = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+        Console.WriteLine(log);
+        sw.WriteLine(log);
+      }
+    }
+  }
+}
